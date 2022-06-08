f8cda5323d7cae53619ff11b9b448625c87e4fd4
Thevenel <47800812+Thevenel@users.noreply.github.com>
2022-06-08 20:33:42 +0200

    created projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
+from operator import mod
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
