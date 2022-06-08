423cf70a42bb46bb9c7d911c7a9c317fe813d90a
Thevenel <47800812+Thevenel@users.noreply.github.com>
2022-06-08 23:38:32 +0200

    created blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
+from datetime import datetime
+
+from django.forms import ImageField
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=300)
+    pub_date = datetime.now()
+    content = models.TextField()
+    blog_img = models.ImageField(upload_to='blog-images/')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+import projects.views
 
 urlpatterns = [
+    path('', projects.views.home, name='home'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
