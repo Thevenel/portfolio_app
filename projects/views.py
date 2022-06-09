59ac8f93d66fc6155f2066423ad5eb5168589d50
Thevenel <47800812+Thevenel@users.noreply.github.com>
2022-06-10 00:50:29 +0200

    showed projects

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . models import Project
 
 # Create your views here.
 def home(request):
-    return render(request, 'projects/index.html')
+    projects = Project.objects
+    return render(request, 'projects/index.html', {'projects': projects})
