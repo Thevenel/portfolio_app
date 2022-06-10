6d318da8ba3cba5ef7d6324db73772bb0bf738d9
Thevenel <47800812+Thevenel@users.noreply.github.com>
2022-06-11 00:17:52 +0200

    displayed all the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from . models import BlogPost
 # Create your views here.
+def Blogpost(request):
+    blogs = BlogPost.objects
+    return render(request, 'blog/blog.html', {'blogs' : blogs})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 import projects.views
+import blog.views
 
 urlpatterns = [
     path('', projects.views.home, name='home'),
+    path('blog/', blog.views.Blogpost, name='blog'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
