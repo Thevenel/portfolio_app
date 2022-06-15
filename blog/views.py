5a5b25de7283e1ec0d2340074024674f590f6637
Thevenel <47800812+Thevenel@users.noreply.github.com>
2022-06-15 23:53:59 +0200

    created blog detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+
+import blog
 from . models import BlogPost
 # Create your views here.
 def allblogs(request):
     blogs = BlogPost.objects
     return render(request, 'blog/blog.html', {'blogs' : blogs})
+
+def detail(request):
+    return render(request, '/blog/blog_detail.html')
