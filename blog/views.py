d5e4b9fb18596750f69e454d25a19150a8d1a3a8
Thevenel <47800812+Thevenel@users.noreply.github.com>
2022-06-16 00:08:25 +0200

    showed the blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-import blog
 from . models import BlogPost
 # Create your views here.
 def allblogs(request):
     blogs = BlogPost.objects
     return render(request, 'blog/blog.html', {'blogs' : blogs})
 
-def detail(request):
-    return render(request, '/blog/blog_detail.html')
+def detail(request, blog_id):
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+    return render(request, 'blog/blog_detail.html', {'blog': blog})
